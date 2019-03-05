572f9bf3b563fd5630e5ff6bacfe5a0e63524384
Kris Read <kris.read@gmail.com>
2019-03-05 12:47:45 -0700

    Clean Up 2019

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const { makeExecutableSchema } = require('graphql-tools');
-const { books, authors } = require('./lib/data');
+const express = require('express')
+const bodyParser = require('body-parser')
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
+const { makeExecutableSchema } = require('graphql-tools')
+const { books, authors } = require('./lib/data')
 
 // The GraphQL schema in string form
 const typeDefs = `
 
   type Query { 
     books: [Book]
+    book(id: Int): Book
   }
   
   type Book {
@@ -20,38 +21,41 @@ const typeDefs = `
     name: String!
   }
 
-`;
+`
 
 // The resolvers
 const resolvers = {
 
-  Query: {
-    books: () => {
-      return Object.values(books);
-    }
+    Query: {
+      books: () => {
+        return Object.values(books)
+      },
+      book: (root, { id }) => {
+        return books[id]
+      }
   },
 
   Book: {
     author: (book) => {
-      return authors[book.author];
+      return authors[book.author]
     }
   }
 
-};
+}
 
 // Put together a schema
-const schema = makeExecutableSchema({ typeDefs, resolvers });
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 // Initialize the app
-const app = express();
+const app = express()
 
 // The GraphQL endpoint
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
 
 // GraphiQL, a visual editor for queries
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Go to http://localhost:3000/graphiql to run queries!');
-});
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Go to http://localhost:3000/graphiql to run queries!')
+})
